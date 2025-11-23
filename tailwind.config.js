e6ad2c82e98b56bbd039fadb7e8df8bc9d5e7b59
Sahil <sahilvishwakarma2004@gmail.com>
2025-11-23 11:06:33 +0530

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        zentry: ['Zentury', 'sans-serif'],
+        general: ['general', 'sans-serif'],
+        'circular-web': ['Circular-web', 'sans-serif'],
+        'robert-medium': ['Robert-medium', 'sans-serif'],
+      },
+      colors: {
+        blue: {
+          50: '#dfdff0',
+          75: '#dfdff2',
+          100:'#f0f2fa',
+          200: '#010101',
+          300: '#4fb7dd',
+        },
+        violet: {
+          300: '#5724ff'
+        },
+        yellow: {
+          100: '#8e983f',
+          300: '#edff66',
+        },
+      }
+    }
+  },
+  plugins: [],
+};
